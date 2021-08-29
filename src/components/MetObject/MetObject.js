98d7156814bf4b5c5e9b554a9c48bf6cea9971b9
Claudette Raynor <sleddd@users.noreply.github.com>
2021-08-29 14:37:20 -0700

    Added useMountedRef hook

--- src/components/MetObject/MetObject.js
+++ src/components/MetObject/MetObject.js
@@ -1,16 +1,20 @@
-import React, { useState, useEffect, Fragment } from "react";
+import React, { useEffect, useState } from "react";
 import Image from "../General/Image/image";
 import NextButton from "../General/Button/NextButton";
 import MoreLink from "../General/Button/MoreLink";
 import testData from "../../../testing/testData";
+import { useMountedRef } from "../../lib/useMountedRef";
 
 export default (props) => {
   const [metData, setMetData] = useState(false);
-  const getRandomObject = () => {
-    //setMetData(testData);
-    //return;
-    let randomNum = Math.random() * 470000;
+  const mounted = useMountedRef();
 
+  const getRandomObject = () => {
+    /*if (mounted.current) {
+      setMetData(testData);
+    }
+    return;*/
+    let randomNum = Math.random() * 470000;
     fetch(
       "https://collectionapi.metmuseum.org/public/collection/v1/objects/" +
         Math.floor(randomNum)
@@ -22,53 +26,50 @@ export default (props) => {
         return response.json();
       })
       .then((data) => {
-        setMetData(data);
+        if (mounted.current) {
+          setMetData(data);
+        }
       });
+    return false;
   };
 
   useEffect(() => {
+    mounted.current = true;
     getRandomObject();
+    return () => (mounted.current = false);
   }, []);
 
   return (
     <div className="metObject">
-      {metData ? (
-        <Fragment>
-          <Image
-            url={metData.primaryImage}
-            alt={metData.objectName}
-            className="col-md-6 pl-5"
-          />
-          <div>
-            <h2>
-              {metData.title && metData.title.length < 1
-                ? metData.objectName
-                : metData.title}
-            </h2>
-            <p>
-              {metData.geographyType} {metData.country}{" "}
-              <span>{metData.objectDate}</span>{" "}
-              {metData.medium
-                ? " from " + metData.medium.toLowerCase() + "."
-                : ""}{" "}
-              {metData.artistDisplayName
-                ? "By " + metData.artistDisplayName + "."
-                : ""}{" "}
-              {metData.department ? metData.department + " of the " : ""}{" "}
-              {metData.repository}.{" "}
-              <span>
-                {metData.creditLine}
-                <br />
-                {metData.culture}
-              </span>
-            </p>
-            <MoreLink link={metData.objectURL} text={"View Details"} />
-            <NextButton onClickHandler={getRandomObject} text="Next Slide" />
-          </div>
-        </Fragment>
-      ) : (
-        ""
-      )}
+      <Image
+        url={metData.primaryImage}
+        alt={metData.objectName}
+        className="col-md-6 pl-5"
+      />
+      <div>
+        <h2>
+          {metData.title && metData.title.length < 1
+            ? metData.objectName
+            : metData.title}
+        </h2>
+        <p>
+          {metData.geographyType} {metData.country}{" "}
+          <span>{metData.objectDate}</span>{" "}
+          {metData.medium ? " from " + metData.medium.toLowerCase() + "." : ""}{" "}
+          {metData.artistDisplayName
+            ? "By " + metData.artistDisplayName + "."
+            : ""}{" "}
+          {metData.department ? metData.department + " of the " : ""}{" "}
+          {metData.repository}.{" "}
+          <span>
+            {metData.creditLine}
+            <br />
+            {metData.culture}
+          </span>
+        </p>
+        <MoreLink link={metData.objectURL} text={"View Details"} />
+        <NextButton onClickHandler={getRandomObject} text="Next Slide" />
+      </div>
     </div>
   );
 };
---
+++ src/lib/useMountedRef.js
@@ -0,0 +1,10 @@
+import { useEffect, useRef } from "react";
+
+export const useMountedRef = () => {
+  const isMounted = useRef(null);
+  useEffect(() => {
+    isMounted.current = true;
+    return () => (isMounted.current = false);
+  });
+  return isMounted;
+};
